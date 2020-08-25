a6d878f37e10adfa320c20976f9fa7355df46744
lucaswang <lucaswang.dev@gmail.com>
2020-08-26 03:59:48 +0800

    [开发] ali-oss upload test

---
+++ aliyun-oss-appserver-java/src/main/java/com/aliyun/oss/appservermaven/App.java
@@ -0,0 +1,40 @@
+package com.aliyun.oss.appservermaven;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		// 开启Server，端口号为8080
+		Server server = new Server();
+		int port = 8080; // 默认端口是：8080
+		if (args.length > 0) {
+			port = Integer.parseInt(args[0]); // 通过命令行参数指定监听 端口。
+		}
+
+		ServerConnector connector = new ServerConnector(server);
+		connector.setPort(port);
+		System.out.println("App server is running on port : " + port);
+
+		server.setConnectors(new Connector[] { connector });
+		// 设置handler根路径。
+		ServletContextHandler webApiContext = new ServletContextHandler();
+		webApiContext.setContextPath("/");
+		webApiContext.addServlet(new ServletHolder(CallbackServer.class), "/*");
+		webApiContext.setSessionHandler(new SessionHandler());
+
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers(new Handler[] { webApiContext, new DefaultHandler() });
+		// 增加handlers并启动server。
+		server.setHandler(handlers);
+		server.start();
+		server.join();
+	}
+}
---
+++ antd-upload-test/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import { Button } from 'antd';
+import AliyunOSSUpload from './AliyunOSSUpload.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AliyunOSSUpload />
+    </div>
+  );
+}
+
+export default App;
